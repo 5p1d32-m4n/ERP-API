c98932243e0fd9f57c06e0d5c83930422f9ab46d
Brandon Baker <brandon.j.baker1011@gmail.com>
2025-01-20 01:39:21 -0400

    Program update

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ErpApi;
+using ErpApi.Data;
+using ErpApi.Interfaces;
+using ErpApi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IProposalRepository, ProposalRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProposalDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
