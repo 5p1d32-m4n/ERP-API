4851ea4f8dd71ebc22dad19237399c7d255902dc
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-01-20 21:16:31 -0400

    set routes for some endpoints.

--- Controllers/ProposalController.cs
+++ Controllers/ProposalController.cs
@@ -6,7 +6,7 @@ namespace ErpApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ProposalController: Controller
+public class ProposalController : Controller
 {
     private readonly IProposalRepository _proposalRepository;
 
@@ -21,18 +21,56 @@ public class ProposalController: Controller
     {
         try
         {
-        var proposals =await _proposalRepository.GetAllProposalsAsync();
-        var properties = proposals.GetType().GetProperties();
-        foreach (var property in properties)
-        {
-            if(property.PropertyType == typeof(Action) || property.PropertyType.Name.Contains("Action"))
+            var proposals = await _proposalRepository.GetAllProposalsAsync();
+            var properties = proposals.GetType().GetProperties();
+            foreach (var property in properties)
             {
-                Console.WriteLine($"Non-serializable property found: {property}");
+                if (property.PropertyType == typeof(Action) || property.PropertyType.Name.Contains("Action"))
+                {
+                    Console.WriteLine($"Non-serializable property found: {property}");
+                }
             }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(proposals);
         }
-        if(!ModelState.IsValid)
-            return BadRequest(ModelState);
-        return Ok(proposals);
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(200, Type = typeof(Proposal))]
+    public async Task<IActionResult> GetProposalById(int id)
+    {
+        try
+        {
+            var proposal = await _proposalRepository.GetProposalByIdAsync(id);
+            if (proposal == null)
+            {
+                return NotFound();
+            }
+            return Ok(proposal);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    [HttpGet]
+    [Route("proposalTypes")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<ProposalType>))]
+    public async Task<IActionResult> GetProposalTypes()
+    {
+        try
+        {
+            var proposalTypes = await _proposalRepository.GetProposalTypesAsync();
+            return Ok(proposalTypes);
         }
         catch (Exception e)
         {
