90c73b6886b8dc5c11a293241180ccc449f79725
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-01-23 19:30:05 -0400

    repo and interface update.

--- Interfaces/IProposalRepository.cs
+++ Interfaces/IProposalRepository.cs
@@ -8,12 +8,13 @@ namespace ErpApi.Interfaces;
 
 public interface IProposalRepository
 {
-    Task<IEnumerable<Proposal>> GetAllProposalsAsync();
+        Task<IEnumerable<Proposal>> GetAllProposalsAsync();
         Task<Proposal> GetProposalByIdAsync(int id);
         Task<int> InsertProposalAsync(Proposal proposal);
         Task<bool> UpdateProposalAsync(Proposal proposal);
         Task<bool> DeleteProposalAsync(int id);
 
+        //Read Interfaces
         Task<IEnumerable<ProposalType>> GetProposalTypesAsync();
         Task<IEnumerable<ProjectType>> GetProjectTypesAsync();
         Task<IEnumerable<ServiceType>> GetServiceTypesAsync();
@@ -33,13 +34,54 @@ public interface IProposalRepository
         Task<IEnumerable<BillingStyle>> GetBillingStylesAsync();
         Task<IEnumerable<ClientVendor>> GetClientsVendorsAsync();
         Task<IEnumerable<ProposalPhase>> GetProposalPhasesAsync();
-
         Task<IEnumerable<Proposal>> GetProposalsByClientIdAsync(int clientId);
-        // Task<IEnumerable<Proposal>> GetProposalsByStatusAsync(string status);
         Task<IEnumerable<Proposal>> GetProposalsByServiceTypeAsync(int serviceTypeId);
         Task<IEnumerable<Proposal>> GetProposalsBySectorAsync(int sectorId);
-
-        // Task<int> GetProposalCountByStatusAsync(string status);
         Task<decimal> GetTotalProposalCostAsync(int proposalId);
         Task<Dictionary<string, int>> GetProposalCountByTypeAsync();
+
+        // Create Interfaces
+        Task<int> InsertProposalTypeAsync(ProposalType proposalType);
+        Task<int> InsertProjectTypeAsync(ProjectType projectType);
+        Task<int> InsertServiceTypeAsync(ServiceType serviceType);
+        Task<int> InsertComplexityAsync(Complexity complexity);
+        Task<int> InsertImpactAsync(Impact impact);
+        Task<int> InsertSectorAsync(Sector sector);
+        Task<int> InsertSectorCategoryAsync(SectorCategory sectorCategory);
+        Task<int> InsertServiceDeliverableCategoryAsync(ServiceDeliverableCategory serviceDeliverableCategory);
+        Task<int> InsertProposalStatusAsync(ProposalStatus proposalStatus);
+        Task<int> InsertStatusOptionAsync(StatusOption statusOption);
+        Task<int> InsertProposalFormatAsync(ProposalFormat proposalFormat);
+        Task<int> InsertProposalResourceAsync(ProjectResource proposalResource);
+        Task<int> InsertAdditionalCostAsync(AdditionalCost additionalCost);
+
+        // Update Interfaces
+        Task<bool> UpdateProposalTypeAsync(ProposalType proposalType);
+        Task<bool> UpdateProjectTypeAsync(ProjectType projectType);
+        Task<bool> UpdateServiceTypeAsync(ServiceType serviceType);
+        Task<bool> UpdateComplexityAsync(Complexity complexity);
+        Task<bool> UpdateImpactAsync(Impact impact);
+        Task<bool> UpdateSectorAsync(Sector sector);
+        Task<bool> UpdateSectorCategoryAsync(SectorCategory sectorCategory);
+        Task<bool> UpdateServiceDeliverableCategoryAsync(ServiceDeliverableCategory serviceDeliverableCategory);
+        Task<bool> UpdateProposalStatusAsync(ProposalStatus proposalStatus);
+        Task<bool> UpdateStatusOptionAsync(StatusOption statusOption);
+        Task<bool> UpdateProposalFormatAsync(ProposalFormat proposalFormat);
+        Task<bool> UpdateProposalResourceAsync(ProjectResource proposalResource);
+        Task<bool> UpdateAdditionalCostAsync(AdditionalCost additionalCost);
+
+        // Delete Interfaces
+        Task<bool> DeleteProposalTypeAsync(int id);
+        Task<bool> DeleteProjectTypeAsync(int id);
+        Task<bool> DeleteServiceTypeAsync(int id);
+        Task<bool> DeleteComplexityAsync(int id);
+        Task<bool> DeleteImpactAsync(int id);
+        Task<bool> DeleteSectorAsync(int id);
+        Task<bool> DeleteSectorCategoryAsync(int id);
+        Task<bool> DeleteServiceDeliverableCategoryAsync(int id);
+        Task<bool> DeleteProposalStatusAsync(int id);
+        Task<bool> DeleteStatusOptionAsync(int id);
+        Task<bool> DeleteProposalFormatAsync(int id);
+        Task<bool> DeleteProposalResourceAsync(int id);
+        Task<bool> DeleteAdditionalCostAsync(int id);
 }
--- Repository/ProposalRepository.cs
+++ Repository/ProposalRepository.cs
@@ -200,4 +200,280 @@ public class ProposalRepository : IProposalRepository
                 .Select(g => new { g.Key, Count = g.Count() })
                 .ToDictionaryAsync(x => x.Key, x => x.Count);
         }
+
+        // Create Methods
+        public async Task<int> InsertProposalTypeAsync(ProposalType proposalType)
+        {
+            _context.ProposalTypes.Add(proposalType);
+            await _context.SaveChangesAsync();
+            return proposalType.Id;
+        }
+
+        public async Task<int> InsertProjectTypeAsync(ProjectType projectType)
+        {
+            _context.ProjectTypes.Add(projectType);
+            await _context.SaveChangesAsync();
+            return projectType.Id;
+        }
+
+        public async Task<int> InsertServiceTypeAsync(ServiceType serviceType)
+        {
+            _context.ServiceTypes.Add(serviceType);
+            await _context.SaveChangesAsync();
+            return serviceType.Id;
+        }
+
+        public async Task<int> InsertComplexityAsync(Complexity complexity)
+        {
+            _context.Complexities.Add(complexity);
+            await _context.SaveChangesAsync();
+            return complexity.Id;
+        }
+
+        public async Task<int> InsertImpactAsync(Impact impact)
+        {
+            _context.Impacts.Add(impact);
+            await _context.SaveChangesAsync();
+            return impact.Id;
+        }
+
+        public async Task<int> InsertSectorAsync(Sector sector)
+        {
+            _context.Sectors.Add(sector);
+            await _context.SaveChangesAsync();
+            return sector.Id;
+        }
+
+        public async Task<int> InsertSectorCategoryAsync(SectorCategory sectorCategory)
+        {
+            _context.SectorCategories.Add(sectorCategory);
+            await _context.SaveChangesAsync();
+            return sectorCategory.Id;
+        }
+
+        public async Task<int> InsertServiceDeliverableCategoryAsync(ServiceDeliverableCategory serviceDeliverableCategory)
+        {
+            _context.ServiceDeliverableCategories.Add(serviceDeliverableCategory);
+            await _context.SaveChangesAsync();
+            return serviceDeliverableCategory.Id;
+        }
+
+        public async Task<int> InsertProposalStatusAsync(ProposalStatus proposalStatus)
+        {
+            _context.ProposalStatuses.Add(proposalStatus);
+            await _context.SaveChangesAsync();
+            return proposalStatus.Id;
+        }
+
+        public async Task<int> InsertStatusOptionAsync(StatusOption statusOption)
+        {
+            _context.StatusOptions.Add(statusOption);
+            await _context.SaveChangesAsync();
+            return statusOption.Id;
+        }
+
+        public async Task<int> InsertProposalFormatAsync(ProposalFormat proposalFormat)
+        {
+            _context.ProposalFormats.Add(proposalFormat);
+            await _context.SaveChangesAsync();
+            return proposalFormat.Id;
+        }
+
+        public async Task<int> InsertProposalResourceAsync(ProjectResource proposalResource)
+        {
+            _context.ProjectResources.Add(proposalResource);
+            await _context.SaveChangesAsync();
+            return proposalResource.Id;
+        }
+
+        public async Task<int> InsertAdditionalCostAsync(AdditionalCost additionalCost)
+        {
+            _context.ProposalAdditionalCosts.Add(additionalCost);
+            await _context.SaveChangesAsync();
+            return additionalCost.Id;
+        }
+
+        // Update Methods
+        public async Task<bool> UpdateProposalTypeAsync(ProposalType proposalType)
+        {
+            _context.ProposalTypes.Update(proposalType);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateProjectTypeAsync(ProjectType projectType)
+        {
+            _context.ProjectTypes.Update(projectType);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateServiceTypeAsync(ServiceType serviceType)
+        {
+            _context.ServiceTypes.Update(serviceType);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateComplexityAsync(Complexity complexity)
+        {
+            _context.Complexities.Update(complexity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateImpactAsync(Impact impact)
+        {
+            _context.Impacts.Update(impact);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateSectorAsync(Sector sector)
+        {
+            _context.Sectors.Update(sector);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateSectorCategoryAsync(SectorCategory sectorCategory)
+        {
+            _context.SectorCategories.Update(sectorCategory);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateServiceDeliverableCategoryAsync(ServiceDeliverableCategory serviceDeliverableCategory)
+        {
+            _context.ServiceDeliverableCategories.Update(serviceDeliverableCategory);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateProposalStatusAsync(ProposalStatus proposalStatus)
+        {
+            _context.ProposalStatuses.Update(proposalStatus);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateStatusOptionAsync(StatusOption statusOption)
+        {
+            _context.StatusOptions.Update(statusOption);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateProposalFormatAsync(ProposalFormat proposalFormat)
+        {
+            _context.ProposalFormats.Update(proposalFormat);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateProposalResourceAsync(ProjectResource proposalResource)
+        {
+            _context.ProjectResources.Update(proposalResource);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAdditionalCostAsync(AdditionalCost additionalCost)
+        {
+            _context.ProposalAdditionalCosts.Update(additionalCost);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        // Delete Methods
+        public async Task<bool> DeleteProposalTypeAsync(int id)
+        {
+            var entity = await _context.ProposalTypes.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProposalTypes.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteProjectTypeAsync(int id)
+        {
+            var entity = await _context.ProjectTypes.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProjectTypes.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteServiceTypeAsync(int id)
+        {
+            var entity = await _context.ServiceTypes.FindAsync(id);
+            if (entity == null) return false;
+            _context.ServiceTypes.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteComplexityAsync(int id)
+        {
+            var entity = await _context.Complexities.FindAsync(id);
+            if (entity == null) return false;
+            _context.Complexities.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteImpactAsync(int id)
+        {
+            var entity = await _context.Impacts.FindAsync(id);
+            if (entity == null) return false;
+            _context.Impacts.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteSectorAsync(int id)
+        {
+            var entity = await _context.Sectors.FindAsync(id);
+            if (entity == null) return false;
+            _context.Sectors.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteSectorCategoryAsync(int id)
+        {
+            var entity = await _context.SectorCategories.FindAsync(id);
+            if (entity == null) return false;
+            _context.SectorCategories.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteServiceDeliverableCategoryAsync(int id)
+        {
+            var entity = await _context.ServiceDeliverableCategories.FindAsync(id);
+            if (entity == null) return false;
+            _context.ServiceDeliverableCategories.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteProposalStatusAsync(int id)
+        {
+            var entity = await _context.ProposalStatuses.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProposalStatuses.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteStatusOptionAsync(int id)
+        {
+            var entity = await _context.StatusOptions.FindAsync(id);
+            if (entity == null) return false;
+            _context.StatusOptions.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteProposalFormatAsync(int id)
+        {
+            var entity = await _context.ProposalFormats.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProposalFormats.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteProposalResourceAsync(int id)
+        {
+            var entity = await _context.ProjectResources.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProjectResources.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteAdditionalCostAsync(int id)
+        {
+            var entity = await _context.ProposalAdditionalCosts.FindAsync(id);
+            if (entity == null) return false;
+            _context.ProposalAdditionalCosts.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
 }
